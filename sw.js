c621441086a4487d1501971e9020f8678bf01bad
feyzanursaka <43739624+feyzanursaka@users.noreply.github.com>
2018-11-28 15:16:01 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+
+const CACHE ='JS'
+function installCB(e) {
+  console.log('install oldu', e.request);
+}
+self.addEventListener('install', installCB)
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('JS', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
